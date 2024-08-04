af0a1600cbb9f6e5887552f80e78bf391c919f5b
Ananth1227 <ananthsundaram007@gmail.com>
2024-08-04 06:22:56 +0530

    Updated

--- Java/Level1/palindrome.java
+++ Java/Level1/palindrome.java
@@ -8,18 +8,13 @@ public class palindrome
         String rev = "";
         System.out.println("Enter a word or a phrase");
         String org =sc.nextLine();
-        System.out.println(org);
         String str1=org.replaceAll("\\p{Punct}","");
-        System.out.println(str1);
         String str2=str1.replaceAll("\\s","");
-        System.out.println(str2);
         int len=str2.length();
-        System.out.println(len);
         for(int i=len-1;i>=0;i--)
         {
             rev+=str2.charAt(i);
         }
-        System.out.println(rev);
         if(str2.equals(rev))
         {
             System.out.println("The string  "+str2+"  is a  palindrome");
